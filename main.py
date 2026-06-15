722d61b6f3b24a72752f55ca7f0cc06501e9ffd3
Bahodur Rasulov <rasulovbahodur2007@gmail.com>
2026-06-15 03:00:13 -0500

    Clean and prep data: filter to washed vs natural, split into X and y

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import pandas as pd
 df = pd.read_csv('arabica_data_cleaned.csv')
-print(df.head())
-print(df.shape)
+#print(df.head())
+#print(df.shape)
+#print(df.columns.tolist())
+data = df[['Aroma', 'Flavor', 'Aftertaste', 'Acidity', 'Body', 'Balance', 'Uniformity', 'Clean.Cup', 'Sweetness', 'Cupper.Points', 'Processing.Method']]
+data = data[data['Processing.Method'].isin(['Washed / Wet', 'Natural / Dry'])]
+data.info()
+print(data['Processing.Method'].value_counts())
+y = data['Processing.Method']
+X = data.drop(columns = ['Processing.Method'])
+print(X.shape)
+print(y.shape)
